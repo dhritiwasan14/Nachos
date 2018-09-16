c0ab91d3dfebe57af41e083633e40ae7dcf9aa85
Dhriti Wasan <dhritiwasan@Dhritis-MacBook-Pro-2.local>
2018-09-16 20:06:53 +0800

    script for reading products

--- scrapper/add_products.py
+++ scrapper/add_products.py
@@ -10,8 +10,17 @@ with open('catelog_data.json', 'r') as f:
     product_dict = json.load(f)
 
     products = db.products
+    count = 1
     for k, v in product_dict.items():
         post_data = {
+            'id': count,
             'name': k, 
-            'content': v['descr']
+            'content': v['description'],
+            'category': v['cateogry'][1:],
+            'price': v['price'],
+            'miles': v['miles'],
+            'image': v['image_source']
         }
+        count+=1
+        product = products.insert_one(post_data)
+        print('One post: {0}'.format(product.inserted_id))
